051596558a26b02240bdec6c99d85ba3730556e7
Aaron Kendrick <akendrick@idauto.net>
2023-06-02 02:25:14 -0500

    New build

--- src/utils/helpers.ts
+++
@@ -1,9 +0,0 @@
-export const getIdFromUrl = (url: string) => {
-    const urlParts = url.split('/')
-    return urlParts[urlParts.length - 2]
-}
-
-export const getResourceTypeFromUrl = (url: string) => {
-    const urlParts = url.split('/')
-    return urlParts[urlParts.length - 3]
-}
